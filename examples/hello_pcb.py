2245d2739fc1996f86af2b3423ce935ea2b253ff
Wayne Keenan <wayne.keenan@gmail.com>
2018-10-21 08:27:40 +0100

    hello world example tidy

--- examples/hello_pcb.py
+++ examples/hello_pcb.py
@@ -1,8 +1,12 @@
-from pcbmodezero import PCB, Line
+from pcbmodezero import PCB
+
+# Hello world example, place an LED in the middle of a 30mm x 30mm PCB
 
 pcb = PCB(board_name ="hello_pcb", boards_dir='../sandpit/boards', width=30, height=30)
 
 pcb.use_library_component('led-1206')
+
 pcb.add_component('led-1206', 'LED1', [0,  0])
+
 pcb.save()
 
