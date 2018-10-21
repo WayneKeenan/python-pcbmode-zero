afb1eca5ec8db22ab5632b9150d696ad098893eb
Wayne Keenan <wayne.keenan@gmail.com>
2018-10-21 08:06:48 +0100

    Use constant for component name

--- examples/procedural_pcb_1.py
+++ examples/procedural_pcb_1.py
@@ -1,11 +1,12 @@
 from math import sin, radians
 from pcbmodezero import PCB
 
+LED = 'led-1206'
 (BOARD_WIDTH, BOARD_HEIGHT) = (60, 60)
 
 pcb = PCB(board_name ="procedural_pcb_1", boards_dir='../sandpit/boards', width = BOARD_WIDTH, height = BOARD_HEIGHT)
 
-pcb.use_library_component('led-1206')
+pcb.use_library_component(LED)
 
 led_id = 0
 x_pos = -BOARD_WIDTH/2 + 10
@@ -13,7 +14,7 @@ x_step = BOARD_WIDTH/10
 
 while x_pos < BOARD_WIDTH/2 - 10 :
     y_pos = sin(radians(x_pos*5)) * BOARD_HEIGHT/2.2
-    pcb.add_component('led-1206', 'LED_' + str(led_id), [x_pos, y_pos])
+    pcb.add_component(LED, 'LED_' + str(led_id), [x_pos, y_pos])
     x_pos += x_step
     led_id +=1
 
