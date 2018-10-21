460ba37f6dea98d9878099ab59595ab843656037
Wayne Keenan <wayne.keenan@gmail.com>
2018-10-21 08:11:25 +0100

    commenting, tisy and simplification of procedural example

--- examples/procedural_pcb_1.py
+++ examples/procedural_pcb_1.py
@@ -1,8 +1,8 @@
 from math import sin, radians
 from pcbmodezero import PCB
 
-LED = 'led-1206'
 (BOARD_WIDTH, BOARD_HEIGHT) = (60, 60)
+LED = 'led-1206'
 
 pcb = PCB(board_name ="procedural_pcb_1", boards_dir='../sandpit/boards', width = BOARD_WIDTH, height = BOARD_HEIGHT)
 
@@ -10,17 +10,20 @@ pcb.use_library_component(LED)
 
 led_id = 0
 x_pos = -BOARD_WIDTH/2 + 10
-x_step = BOARD_WIDTH/10
+
+# Create a sine wave arrangement of LED parts on the PCB
 
 while x_pos < BOARD_WIDTH/2 - 10 :
     y_pos = sin(radians(x_pos*5)) * BOARD_HEIGHT/2.2
-    pcb.add_component(LED, 'LED_' + str(led_id), [x_pos, y_pos])
-    x_pos += x_step
+    pcb.add_component(LED, 'LED_%d' % led_id, [x_pos, y_pos])
+    x_pos += BOARD_WIDTH/10
+
     led_id +=1
 
+# Connect each LED pins 1 and 2 to the previous LED pins in list.
 
 for led_id in range(1, led_id):
-    pcb.route_pins('LED_' + str(led_id-1), '1', 'LED_' + str(led_id), '1')
-    pcb.route_pins('LED_' + str(led_id-1), '2', 'LED_' + str(led_id), '2', layer='top')
+    pcb.route_pins('LED_%d' % (led_id-1), '1', 'LED_%d' % led_id, '1')
+    pcb.route_pins('LED_%d' % (led_id-1), '2', 'LED_%d' % led_id, '2', layer='top')
 
 pcb.save()
